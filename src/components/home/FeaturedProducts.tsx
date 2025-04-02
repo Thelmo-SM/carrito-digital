7fa343abd49e08070f0046c0e57f94dc3f06040c
Thelmo-SM <toraphebi@gmail.com>
2025-04-01 21:55:01 -0400

    Lazy loading en el home

--- src/components/home/FeaturedProducts.tsx
+++ src/components/home/FeaturedProducts.tsx
@@ -1,69 +1,106 @@
 'use client';
 
-import { useState, useEffect } from "react";
-import { getTopRatedProducts } from "@/utils/firebase";  // Asegúrate de importar esta función
+import { useState, useEffect, useCallback } from "react";
+import { getTopRatedProducts } from "@/utils/firebase";
 import Style from '@/styles/products.module.css';
 import Image from "next/image";
 import Link from "next/link";
 import { formatPrice } from "@/features/Dashboard/helpers/formatPrice";
 import { productsTypes } from "@/types/productTypes";
-import FeaturedCategories from "./FeaturedCategories";  // Asegúrate de importar el componente FeaturedCategories
+import FeaturedCategories from "./FeaturedCategories";
+import { LoaderUi } from "../UI/LoaderUi";
 
 export const FeaturedProducts = () => {
-    const [products, setProducts] = useState<productsTypes[]>([]);
-    const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-  
-    const onSelectCategory = (category: string | null) => {
-      setSelectedCategory(category);
-    };
-  
-    useEffect(() => {
-      const fetchTopRatedProducts = async () => {
-        const data = await getTopRatedProducts();
-        if (selectedCategory) {
-          const filteredData = data.filter((product) =>
-            product.categorie.includes(selectedCategory) // Verifica si la categoría está en el array
-          );
-          setProducts(filteredData);
-        } else {
-          setProducts(data);
-        }
-      };
-  
-      fetchTopRatedProducts();
-    }, [selectedCategory]);
-  
-    return (
-      <div>
-        <FeaturedCategories 
-          onSelectCategory={onSelectCategory} 
-          selectedCategory={selectedCategory} 
-        />
-        <div className={Style.container}>
-          {products.map((product) => (
-            <div key={product.id} className={Style.cardContainer}>
-              {product.imageUrl ? (
-                <Image
-                  src={product.imageUrl}
-                  width={150}
-                  height={100}
-                  alt={product.name}
-                  className={Style.img}
-                />
-              ) : (
-                <p>No hay imagen disponible</p>
-              )}
-              <p className={Style.title1}>{product.name}</p>
-              <p className={Style.price}>{formatPrice(Number(product.price))}</p>
-              <p>Cantidad - <span className={Style.span}>{product.soldUnits}</span></p>
-              <p>Rating: {product.avgRating}</p>
-              <Link href={`/products/${product.id}`} className={Style.detalle}>Ver detalles</Link>
-              <button className={Style.button}>AÑADIR AL CARRITO</button>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
+  const [products, setProducts] = useState<productsTypes[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [visibleProducts, setVisibleProducts] = useState<productsTypes[]>([]);  // Productos que se van a mostrar
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1); // Página actual de productos
+
+  const productsPerPage = 2;  // Número de productos por "página"
+
+  const onSelectCategory = (category: string | null) => {
+    setSelectedCategory(category);
+    setCurrentPage(1);  // Reseteamos la página cuando se cambia de categoría
   };
-  
-  export default FeaturedProducts;
+
+  const loadProducts = useCallback(async () => {
+    setLoading(true);
+    const data = await getTopRatedProducts();
+    if (selectedCategory) {
+      const filteredData = data.filter((product) =>
+        product.categorie.includes(selectedCategory) // Filtramos por categoría
+      );
+      setProducts(filteredData);
+    } else {
+      setProducts(data);
+    }
+    setLoading(false);
+  }, [selectedCategory]);
+
+  // Función para cargar más productos
+  const loadMoreProducts = () => {
+    setCurrentPage((prevPage) => prevPage + 1);  // Incrementamos la página
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, [loadProducts]);
+
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * productsPerPage;
+    const endIndex = startIndex + productsPerPage;
+    setVisibleProducts((prevProducts) => [
+      ...prevProducts,
+      ...products.slice(startIndex, endIndex),
+    ]);
+  }, [currentPage, products]);
+
+  return (
+    <div className={Style.article}>
+      <FeaturedCategories 
+        onSelectCategory={onSelectCategory} 
+        selectedCategory={selectedCategory} 
+      />
+      <div className={Style.container}>
+        {visibleProducts.map((product) => (
+          <div key={product.id} className={Style.cardContainer}>
+            {product.imageUrl ? (
+              <Image
+                src={product.imageUrl}
+                width={150}
+                height={100}
+                alt={product.name}
+                className={Style.img}
+              />
+            ) : (
+              <p>No hay imagen disponible</p>
+            )}
+            <p className={Style.title1}>{product.name}</p>
+            <p className={Style.price}>{formatPrice(Number(product.price))}</p>
+            <p>Cantidad - <span className={Style.span}>{product.soldUnits}</span></p>
+            <p>Rating: {product.avgRating}</p>
+            <Link href={`/products/${product.id}`} className={Style.detalle}>Ver detalles</Link>
+            <button className={Style.button}>AÑADIR AL CARRITO</button>
+          </div>
+        ))}
+      </div>
+      
+      {/* Mostrar el botón de cargar más productos */}
+      <div className={Style.loadMoreButtonContainer}>
+        {loading ? (
+          <div className={Style.loading}>
+            <LoaderUi />
+            <p>Cargando productos...</p>
+          </div>
+        ) : (
+          <button onClick={loadMoreProducts} className={Style.loadMoreButton}>
+           Cargar más
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
