a7073b3de7f146362779ead91dad77e57a323d1f
Thelmo-SM <toraphebi@gmail.com>
2025-04-27 19:40:42 -0400

    Preparando la aplicacion para subirla a vercel

--- src/utils/verifyToken.ts
+++ src/utils/verifyToken.ts
@@ -1,5 +1,3 @@
-//import Cookies from 'js-cookie';
-
 export const verifyTokenAPI = async (token: string) => {
   try {
     const res = await fetch('/api/verify-token', {
@@ -10,10 +8,22 @@ export const verifyTokenAPI = async (token: string) => {
       body: JSON.stringify({ token }),
     });
 
+    // Verifica si la respuesta fue exitosa
+    if (!res.ok) {
+      console.error('Error: El servidor respondió con un error:', res.status);
+      return false;
+    }
+
     const data = await res.json();
-    return data.success; // Retorna true si el token es válido, de lo contrario false
+
+    if (data.success) {
+      return true; // Token válido
+    } else {
+      console.error('Error: Token inválido o no encontrado.');
+      return false; // Token inválido
+    }
   } catch (error) {
     console.error('Error al verificar el token:', error);
-    return false;
+    return false; // Si hubo un error en la solicitud o en el servidor
   }
 };
